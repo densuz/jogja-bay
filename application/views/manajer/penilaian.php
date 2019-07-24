60695775dd1e8ca7f838e0e2b4d3c0074574057c
csynoers <3s0c9m7@gmail.com>
2019-07-24 16:24:59 +0700

    debug

--- application/views/manajer/penilaian.php
+++ application/views/manajer/penilaian.php
@@ -13,78 +13,44 @@
                           <tr>
                               <th>Name</th>
                               <th>Divisi</th>
-                              <th style="min-width: 120px">Attitude</th>
-                              <th style="min-width: 120px">Grooming</th>
-                              <th style="min-width: 120px">Ketelitian</th>
-                              <th style="min-width: 120px">Service Excellent</th>
-                              <th style="min-width: 120px">Personal Selling</th>
-                              <th style="min-width: 120px">Kerjasama Tim</th>
-                              <th style="min-width: 120px">Kedisiplinan</th>
-                              <th style="min-width: 120px">Kejujuran</th>
-                              <th style="min-width: 120px">Inisiatif</th>
+                              <?php
+                                $thead= '';
+                                foreach ($kriteria as $key => $value) {
+                                  $thead .= '
+                                    <th style="min-width: 120px">'.$value->nama_kriteria.'</th>
+                                  '
+                                }
+                                echo $thead;
+                              ?>
                           </tr>
                       </thead>
                       <tbody>
                           <?php
-                            echo "<pre>";
-                            print_r($karyawan);
-                            print_r($kriteria);
-                            echo "</pre>";
-                              $data =[
-                                  [
-                                      'id_user'       => 1,
-                                      'nama'          => 'Karyawan 1',
-                                      'nama_divisi'   => 'Pramuniaga',
-                                  ],
-                                  [
-                                      'id_user'       => 2,
-                                      'nama'          => 'Karyawan 2',
-                                      'nama_divisi'   => 'Kasir',
-                                  ],
-                                  [
-                                      'id_user'       => 3,
-                                      'nama'          => 'Karyawan 3',
-                                      'nama_divisi'   => 'Security',
-                                  ],
-                                  [
-                                      'id_user'       => 4,
-                                      'nama'          => 'Karyawan 4',
-                                      'nama_divisi'   => 'Logistik',
-                                  ],
-                              ];
-                              foreach ($karyawan as $key => $value) {
-                                $form_sub= '';
-                                foreach ($kriteria as $key_sub => $value_sub) {
-                                  $form_sub .= '
-                                    <td>
-                                        <input required="" value="" type="number" name="id_kriteria_'.$value->id_user.'['.$value_sub->id_kriteria.']" placeholder="0.1 s.d 1" step="0.1" min="0.1" max="1" class="form-control" >
-                                    </td>
-                                  ';
-                                }
-                                  echo '
-                                      <tr>
-                                          <input class="id_user" name="id_user[]" type="hidden" value="'.$value->id_user.'">
-                                          <td>'.$value->nama.'</td>
-                                          <td>'.$value->nama_divisi.'</td>
-                                          '.$form_sub.'
-                                      </tr>
-                                  ';
+                            foreach ($karyawan as $key => $value) {
+                              $form_sub= '';
+                              foreach ($kriteria as $key_sub => $value_sub) {
+                                $form_sub .= '
+                                  <td>
+                                      <input required="" value="" type="number" name="id_kriteria_'.$value->id_user.'['.$value_sub->id_kriteria.']" placeholder="0.1 s.d 1" step="0.1" min="0.1" max="1" class="form-control" >
+                                  </td>
+                                ';
                               }
+                                echo '
+                                    <tr>
+                                        <input class="id_user" name="id_user[]" type="hidden" value="'.$value->id_user.'">
+                                        <td>'.$value->nama.'</td>
+                                        <td>'.$value->nama_divisi.'</td>
+                                        '.$form_sub.'
+                                    </tr>
+                                ';
+                            }
                           ?>
                       </tbody>
                       <tfoot>
                           <tr>
                               <th>Name</th>
                               <th>Divisi</th>
-                              <th>Attitude</th>
-                              <th>Grooming</th>
-                              <th>Ketelitian</th>
-                              <th>Service Excellent</th>
-                              <th>Personal Selling</th>
-                              <th>Kerjasama Tim</th>
-                              <th>Kedisiplinan</th>
-                              <th>Kejujuran</th>
-                              <th>Inisiatif</th>
+                              <?php echo $thead; ?>
                           </tr>
                       </tfoot>
                   </table>
