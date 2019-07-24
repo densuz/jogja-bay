15645b81540e4a66f3790f2bacbcf7c19d63d620
csynoers <3s0c9m7@gmail.com>
2019-07-24 11:34:21 +0700

    clear http request

--- application/views/auth.php
+++ application/views/auth.php
@@ -8,13 +8,13 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="themes/adminlte/maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="<?php echo base_url() ?>themes/adminlte/maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
   <!-- Ionicons -->
-  <link rel="stylesheet" href="themes/adminlte/code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <link rel="stylesheet" href="<?php echo base_url() ?>themes/adminlte/code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="themes/adminlte/adminlte.io/themes/dev/adminlte/dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="<?php echo base_url() ?>themes/adminlte/adminlte.io/themes/dev/adminlte/dist/css/adminlte.min.css">
   <!-- iCheck -->
-  <link rel="stylesheet" href="themes/adminlte/adminlte.io/themes/dev/adminlte/plugins/iCheck/square/blue.css">
+  <link rel="stylesheet" href="<?php echo base_url() ?>themes/adminlte/adminlte.io/themes/dev/adminlte/plugins/iCheck/square/blue.css">
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
