d9019bb32d59879ebfd23f4d7629a5b827c7b1de
csynoers <3s0c9m7@gmail.com>
2019-07-24 16:31:10 +0700

    debug

--- application/views/manajer/penilaian.php
+++ application/views/manajer/penilaian.php
@@ -1,4 +1,20 @@
 <div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1>Penilaian Karyawan</h1>
+        </div>
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="<?php echo base_url( $this->session->userdata('level') ) ?>">Beranda</a></li>
+            <li class="breadcrumb-item active">Penilaian Karyawan</li>
+          </ol>
+        </div>
+      </div>
+    </div><!-- /.container-fluid -->
+  </section>
   <!-- Main content -->
   <section class="content">
     <div class="row">
@@ -11,6 +27,7 @@
                   <table id="example1" class="table table-bordered table-striped">
                       <thead>
                           <tr>
+                              <th>No</th>
                               <th>Name</th>
                               <th>Divisi</th>
                               <?php
@@ -26,6 +43,7 @@
                       </thead>
                       <tbody>
                           <?php
+                            $no= 1;
                             foreach ($karyawan as $key => $value) {
                               $form_sub= '';
                               foreach ($kriteria as $key_sub => $value_sub) {
@@ -35,23 +53,26 @@
                                   </td>
                                 ';
                               }
-                                echo '
-                                    <tr>
-                                        <input class="id_user" name="id_user[]" type="hidden" value="'.$value->id_user.'">
-                                        <td>'.$value->nama.'</td>
-                                        <td>'.$value->nama_divisi.'</td>
-                                        '.$form_sub.'
-                                    </tr>
-                                ';
+                              echo '
+                                  <tr>
+                                      <input class="id_user" name="id_user[]" type="hidden" value="'.$value->id_user.'">
+                                      <td>'.$no.'</td>
+                                      <td>'.$value->nama.'</td>
+                                      <td>'.$value->nama_divisi.'</td>
+                                      '.$form_sub.'
+                                  </tr>
+                              ';
+                              $no++;
                             }
                           ?>
                       </tbody>
                       <tfoot>
-                          <tr>
-                              <th>Name</th>
-                              <th>Divisi</th>
-                              <?php echo $thead; ?>
-                          </tr>
+                        <tr>
+                          <th>No</th>
+                          <th>Name</th>
+                          <th>Divisi</th>
+                          <?php echo $thead; ?>
+                        </tr>
                       </tfoot>
                   </table>
               </div>
