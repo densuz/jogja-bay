9fc25e08e0930c36907d5837d659c96010ddc96f
csynoers <3s0c9m7@gmail.com>
2019-07-25 16:48:26 +0700

    debug

--- application/views/hrd/hasil_akhir.php
+++ application/views/hrd/hasil_akhir.php
@@ -78,6 +78,18 @@
                                 <td>{$value->nama_divisi}</td>
                                 <td>0.7(contoh)</td>
                                 <td>0.8(contoh)</td>
+                                <td>
+                                  <div class='btn-group'>
+                                    <button type='button' class='btn btn-default'>Action</button>
+                                    <button type='button' class='btn btn-default dropdown-toggle' data-toggle='dropdown' aria-expanded='false'>
+                                      <span class='caret'></span>
+                                      <span class='sr-only'>Toggle Dropdown</span>
+                                    </button>
+                                    <div class='dropdown-menu' role='menu' x-placement='top-start' style='position: absolute; will-change: transform; top: 0px; left: 0px; transform: translate3d(67px, -165px, 0px);'>
+                                      <a class='dropdown-item form-load' href='".base_url( $this->session->userdata('level') .'/detail-hasil-akhir/' .$value->id_user)."' title='Edit Informasi Karyawan'>Edit</a>
+                                    </div>
+                                  </div>
+                                </td>
                               </tr>
                             ";
                             $no++;
