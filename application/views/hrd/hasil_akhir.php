7592ed223a18f93bfbde3cbea7a97098b9a63d3e
csynoers <3s0c9m7@gmail.com>
2019-07-25 14:32:02 +0700

    debug

--- application/views/hrd/hasil_akhir.php
+++ application/views/hrd/hasil_akhir.php
@@ -19,12 +19,12 @@
     </div><!-- /.container-fluid -->
   </section>
   <?php
-  // echo '<pre>';
-  // print_r($karyawan);
+  echo '<pre>';
+  print_r($karyawan);
   // print_r($kriteria);
   // print_r($penilaian);
   // print_r($duplicate);
-  // echo '</pre>';
+  echo '</pre>';
   ?>
   <!-- Main content -->
   <section class="content">
