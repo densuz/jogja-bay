492be19744ff4e3138ee263c99d4c11d6d342779
csynoers <3s0c9m7@gmail.com>
2019-07-31 01:26:57 +0700

    debug

--- application/controllers/Hrd.php
+++ application/controllers/Hrd.php
@@ -497,15 +497,17 @@
         public function detail_hasil_akhir()
         {
             $this->data->rows= [] ;
+            $this->data->tahun_penilaian= $this->M_hrd->tahun_penilaian();
+            $this->data->bulan_penilaian= $this->M_hrd->bulan_penilaian();
             $this->data->penilaian_per_bulan= $this->M_hrd->show_penilaian_distinct_tanggal();
             $this->data->kriteria= $this->M_hrd->show_kriteria();
 
             /* loop data berdasarkan tanggal penilaian  perbulan*/
-            foreach ($this->data->penilaian_per_bulan as $key => $value) {
-                $this->data->rows[]= [
-                    'bulan' => $value,
-                ];
-            }
+            // foreach ($this->data->penilaian_per_bulan as $key => $value) {
+            //     $this->data->rows[]= [
+            //         'bulan' => $value,
+            //     ];
+            // }
             echo '<pre>';
             print_r($this->data);
             echo '</pre>';
