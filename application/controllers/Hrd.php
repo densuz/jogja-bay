1d1a5b1c5d2334088cfd34e431800ebc06341d33
csynoers <3s0c9m7@gmail.com>
2019-07-31 14:04:30 +0700

    debug

--- application/controllers/Hrd.php
+++ application/controllers/Hrd.php
@@ -653,7 +653,7 @@
         {
             $rows=[];
             foreach ($this->M_hrd->show_karyawan() as $key => $value) {
-                $row[]=$value;
+                $rows[]=$value;
             }
             echo '<pre>';
             print_r($rows);
