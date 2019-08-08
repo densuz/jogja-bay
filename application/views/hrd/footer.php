0f849ff80b519e702f3a014b703de1de6f19cdd8
csynoers <3s0c9m7@gmail.com>
2019-08-08 11:38:37 +0700

    tambah periode pada halaman hasil akhir hrd

--- application/views/hrd/footer.php
+++ application/views/hrd/footer.php
@@ -153,6 +153,17 @@
         dataType: 'json'
     });
   });
+  $(function() {
+    $('.date-picker').datepicker( {
+        changeMonth: true,
+        changeYear: true,
+        showButtonPanel: true,
+        dateFormat: 'MM yy',
+        onClose: function(dateText, inst) { 
+            $(this).datepicker('setDate', new Date(inst.selectedYear, inst.selectedMonth, 1));
+        }
+    });
+  });
 </script>
 </body>
 
