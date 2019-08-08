73f7e2d8a5ddaf0b3656c660c3a17e39e606b5ad
csynoers <3s0c9m7@gmail.com>
2019-08-08 16:42:57 +0700

    tambah periode pada halaman hasil akhir hrd v.22

--- application/views/hrd/hasil_akhir.php
+++ application/views/hrd/hasil_akhir.php
@@ -129,13 +129,13 @@
                               $nilai_total= 0;
                               $nilai_rows= count($bulan_penilaian);
                               foreach ($nilai as $key_nilai => $value_nilai) {
-                                if ( ($value_nilai['tahun']==$value_mod->tahun_penilaian) && $value_nilai['id_bulan']==$value_mod->id_bulan ) {
-                                  $tes .= '<td>'.$value_nilai['nilai'].'</td>';
-                                  $nilai_total += $value_nilai['nilai'];
-                                } else {
-                                  $tes .= '<td>0</td>';
-                                  $nilai_total += 0;
-                                }
+                                // if ( ($value_nilai['tahun']==$value_mod->tahun_penilaian) && $value_nilai['id_bulan']==$value_mod->id_bulan ) {
+                                //   $tes .= '<td>'.$value_nilai['nilai'].'</td>';
+                                //   $nilai_total += $value_nilai['nilai'];
+                                // } else {
+                                //   $tes .= '<td>0</td>';
+                                //   $nilai_total += 0;
+                                // }
                                 
                               }
                               $nilai_mean= ($nilai_total/$nilai_rows);
