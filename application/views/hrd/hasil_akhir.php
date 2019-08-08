5b737b26755a044fcd91c8cba6a77d6bd08369e2
csynoers <3s0c9m7@gmail.com>
2019-08-08 11:41:03 +0700

    tambah periode pada halaman hasil akhir hrd v.1

--- application/views/hrd/hasil_akhir.php
+++ application/views/hrd/hasil_akhir.php
@@ -28,108 +28,106 @@
     <div class="row">
       <div class="col-12">
         <div class="card">
-          <form action="<?php echo base_url( $this->session->userdata('level') ) ?>/store-penilaian" method="post" class="data-store">
-              <!-- /.card-header -->
-              <div class="card-body">
-                <form class="form-inline" action="">
-                  <label>Pilih Periode : </label>
-                  <input name="start_date" id="startDate" class="date-picker form-control" />
-                  <label>Sampai</label>
-                  <input name="end_date" id="endDate" class="date-picker form-control" />
-                  <button type="submit" class="btn btn-primary">Submit</button>
-                </form>
-                <div class="table-responsive">
-                    <table id="example1" class="table table-bordered table-striped">
-                        <thead class="text-center">
-                          <?php
-                            $th_tahun= '';
-                            $tr_bulan= '';
-                            $th_bulan= '';
-                            foreach ($tahun_penilaian as $key => $value) {
-                              $col_tahun= 0;
-                              foreach ($bulan_penilaian as $key_bulan => $value_bulan) {
-                                if ( $value_bulan->tahun_penilaian==$value->tahun_penilaian ) {
-                                  $col_tahun++;
-                                }
-                                $th_bulan .= "<th>{$value_bulan->bulan_penilaian}</th>";
-                              }
-                              $th_tahun .= "<th colspan='{$col_tahun}'>Tahun {$value->tahun_penilaian}</th>";
+          <!-- /.card-header -->
+          <div class="card-body">
+            <form class="form-inline" action="">
+              <label>Pilih Periode : </label>
+              <input name="start_date" id="startDate" class="date-picker form-control" />
+              <label>Sampai</label>
+              <input name="end_date" id="endDate" class="date-picker form-control" />
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+            <div class="table-responsive">
+                <table id="example1" class="table table-bordered table-striped">
+                    <thead class="text-center">
+                      <?php
+                        $th_tahun= '';
+                        $tr_bulan= '';
+                        $th_bulan= '';
+                        foreach ($tahun_penilaian as $key => $value) {
+                          $col_tahun= 0;
+                          foreach ($bulan_penilaian as $key_bulan => $value_bulan) {
+                            if ( $value_bulan->tahun_penilaian==$value->tahun_penilaian ) {
+                              $col_tahun++;
                             }
+                            $th_bulan .= "<th>{$value_bulan->bulan_penilaian}</th>";
+                          }
+                          $th_tahun .= "<th colspan='{$col_tahun}'>Tahun {$value->tahun_penilaian}</th>";
+                        }
 
-                            echo "
-                              <tr>
-                                <th rowspan='2'>No</th>
-                                <th rowspan='2'>Name</th>
-                                <th rowspan='2'>Divisi</th>
-                                {$th_tahun}
-                                <th colspan='2'>Nilai</th>
-                                <th rowspan='2'>&nbsp</th>
-                              </tr>
-                              <tr>
-                                {$th_bulan}
-                                <th>Total</th>
-                                <th>Mean(rata-rata)</th>
-                              </tr>
-                            ";
-                          ?>
-                        </thead>
-                        <tbody>
-                          <?php
-                            $no = 1;
-                            $tbody= '';
-                            foreach ($karyawan as $key => $value) {
-                              /* start generate nilai saw perbulan */
-                              $tes='';
-                              $nilai= $hasil_per_bulan[$value->id_user]['penilaian'];
-                              $nilai_total= 0;
-                              $nilai_rows= count($nilai);
-                              foreach ($nilai as $key_nilai => $value_nilai) {
-                                $tes .= '<td>'.$value_nilai['nilai'].'</td>';
-                                $nilai_total += $value_nilai['nilai'];
-                              }
-                              $nilai_mean= ($nilai_total/$nilai_rows);
-                              /* end generate nilai saw perbulan */
-
-                              $tbody .= "
-                                <tr>
-                                  <td>{$no}</td>
-                                  <td>{$value->nama}</td>
-                                  <td>{$value->nama_divisi}</td>
-                                  {$tes}
-                                  <td>{$nilai_total}</td>
-                                  <td>{$nilai_mean}</td>
-                                  <td>
-                                    <div class='btn-group'>
-                                      <button type='button' class='btn btn-default'>Action</button>
-                                      <button type='button' class='btn btn-default dropdown-toggle' data-toggle='dropdown' aria-expanded='false'>
-                                        <span class='caret'></span>
-                                        <span class='sr-only'>Toggle Dropdown</span>
-                                      </button>
-                                      <div class='dropdown-menu' role='menu' x-placement='top-start' style='position: absolute; will-change: transform; top: 0px; left: 0px; transform: translate3d(67px, -165px, 0px);'>
-                                        <a class='dropdown-item form-load' href='".base_url( $this->session->userdata('level') .'/detail-hasil-akhir/' .$value->id_user)."' title='Detail Hasil Akhir'>Detail Nilai Akhir</a>
-                                      </div>
-                                    </div>
-                                  </td>
-                                </tr>
-                              ";
-                              $no++;
-                            }
-                            echo $tbody;
-                          ?>
-                        </tbody>
-                        <!-- <tfoot>
+                        echo "
                           <tr>
-                            <th>No</th>
-                            <th>Name</th>
-                            <th>Divisi</th>
-                            <th>Tanggal&nbspPenilaian</th>
+                            <th rowspan='2'>No</th>
+                            <th rowspan='2'>Name</th>
+                            <th rowspan='2'>Divisi</th>
+                            {$th_tahun}
+                            <th colspan='2'>Nilai</th>
+                            <th rowspan='2'>&nbsp</th>
                           </tr>
-                        </tfoot> -->
-                    </table>
-                </div>
-              </div>
-              <!-- /.card-body -->
-          </form>
+                          <tr>
+                            {$th_bulan}
+                            <th>Total</th>
+                            <th>Mean(rata-rata)</th>
+                          </tr>
+                        ";
+                      ?>
+                    </thead>
+                    <tbody>
+                      <?php
+                        $no = 1;
+                        $tbody= '';
+                        foreach ($karyawan as $key => $value) {
+                          /* start generate nilai saw perbulan */
+                          $tes='';
+                          $nilai= $hasil_per_bulan[$value->id_user]['penilaian'];
+                          $nilai_total= 0;
+                          $nilai_rows= count($nilai);
+                          foreach ($nilai as $key_nilai => $value_nilai) {
+                            $tes .= '<td>'.$value_nilai['nilai'].'</td>';
+                            $nilai_total += $value_nilai['nilai'];
+                          }
+                          $nilai_mean= ($nilai_total/$nilai_rows);
+                          /* end generate nilai saw perbulan */
+
+                          $tbody .= "
+                            <tr>
+                              <td>{$no}</td>
+                              <td>{$value->nama}</td>
+                              <td>{$value->nama_divisi}</td>
+                              {$tes}
+                              <td>{$nilai_total}</td>
+                              <td>{$nilai_mean}</td>
+                              <td>
+                                <div class='btn-group'>
+                                  <button type='button' class='btn btn-default'>Action</button>
+                                  <button type='button' class='btn btn-default dropdown-toggle' data-toggle='dropdown' aria-expanded='false'>
+                                    <span class='caret'></span>
+                                    <span class='sr-only'>Toggle Dropdown</span>
+                                  </button>
+                                  <div class='dropdown-menu' role='menu' x-placement='top-start' style='position: absolute; will-change: transform; top: 0px; left: 0px; transform: translate3d(67px, -165px, 0px);'>
+                                    <a class='dropdown-item form-load' href='".base_url( $this->session->userdata('level') .'/detail-hasil-akhir/' .$value->id_user)."' title='Detail Hasil Akhir'>Detail Nilai Akhir</a>
+                                  </div>
+                                </div>
+                              </td>
+                            </tr>
+                          ";
+                          $no++;
+                        }
+                        echo $tbody;
+                      ?>
+                    </tbody>
+                    <!-- <tfoot>
+                      <tr>
+                        <th>No</th>
+                        <th>Name</th>
+                        <th>Divisi</th>
+                        <th>Tanggal&nbspPenilaian</th>
+                      </tr>
+                    </tfoot> -->
+                </table>
+            </div>
+          </div>
+          <!-- /.card-body -->
         </div>
         <!-- /.card -->
       </div>
